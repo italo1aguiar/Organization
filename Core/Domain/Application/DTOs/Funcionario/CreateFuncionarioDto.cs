20ce67a65216e0ef6185d1ab4432955649da0914
italoaguiar <italo_aguiar@alu.ufc.br>
2022-03-21 18:08:12 -0300

    mais uma

---
+++ Core/Domain/Application/DTOs/Funcionario/CreateFuncionarioDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Application.DTOs.Funcionario
+{
+	public class CreateFuncionarioDto
+	{
+		public string Nome { get; set; }
+		public string Cpf { get; set; }
+		public string Rg { get; set; }
+		public DateTime DataNascimento { get; set; }
+		public string Mail { get; set; }
+		public string Telefone { get; set; }
+	}
+}
+
